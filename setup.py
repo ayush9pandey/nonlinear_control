a87cadcfd9a5deeb13c5c00b98c67e17910f2bc1
ayush9pandey <ayush.9.pandey@gmail.com>
2019-08-02 16:56:59 -0700

    added license, python setuptools, and appropriate directory structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+# Get the long description from the README file
+with open('README.md') as fp:
+    long_description = ''
+    # long_description = fp.read()
+
+setup(
+    name = 'nonlinear-control',
+    version = '0.1',
+    author = 'Ayush Pandey',
+    author_email = 'apandey@caltech.edu',
+    url = 'https://github.com/ayush9pandey/nonlinear-control',
+    description = 'Nonlinear control toolbox in Python',
+    long_description = long_description,
+    packages = ['nonlinear-control'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: POSIX',
+        'Operating System :: Unix'
+        'Operating System :: MacOS'
+        'Operating System :: Microsoft :: Windows'
+    ],
+)
